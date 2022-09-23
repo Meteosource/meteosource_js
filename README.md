ea917c95a3d02f75752ea497bd403f2e0a28e73d
Meteosource <support@meteosource.com>
2022-09-23 13:07:11 +0200

    First version

---
+++ README.md
@@ -0,0 +1,213 @@
+meteosource - Weather API library for Javascript
+==========
+
+Javascript wrapper library for [Meteosource weather API](https://www.meteosource.com) that provides detailed hyperlocal weather forecasts for any location on earth. The library supports both Web and Node.JS runtimes.
+
+
+### Installation: web
+
+The source code is available in the file ``meteosource.js`` and requires the datetime library ``luxon``.
+
+```HTML
+<script src="https://www.meteosource.com/TODO/meteosource.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/luxon@2.4.0/build/global/luxon.min.js"></script>
+
+<script>document.write(meteosource.version)</script> <!-- returns 0.1 -->
+```
+
+### Installation: NPM (Node.js)
+
+To add the library to your project, run the following command in the directory tree of the project:
+
+```shell
+$ npm install git+ssh://git@gitlab.meteocentrum.cz:23/kuba.marousek/meteosource_js.git
+$ node -p 'require("meteosource").version'
+0.1
+```
+
+
+### Get started
+
+To use this library, you need to obtain your Meteosource API key. You can [sign up](https://www.meteosource.com/client/sign-up) or get the API key of existing account in [your dashboard](https://www.meteosource.com/client).
+
+# Library usage
+
+## Initialization
+
+To initialize the `Meteosource` object, you need your API key and the name of your subscription plan (tier). Basic example of initialization is shown below:
+
+```javascript
+let apiKey = 'abcdefghijklmnopqrstuvwxyz0123456789ABCD'
+let tier = 'flexi'
+
+let m = new meteosource.Meteosource(apiKey, tier)
+```
+
+## Get the weather data
+
+Using the library, you can get weather forecasts or archive weather data (if you have a paid subscription).
+
+### Forecast
+To get the weather data for given place, use `getPointForecast()` async method of the `Meteosource` object. You have to specify either the coordinates of the place (`lat` + `lon`) or the `placeId`. Detailed description of the parameters can be found in the [API documentation](https://www.meteosource.com/documentation).
+
+Note that the default timezone is always `UTC`, as opposed to the API itself (which defaults to the point's local timezone). This is because the library always queries the API for the `UTC` timezone to avoid ambiguous datetimes problems. If you specify the ``tz`` parameter to specify a different timezone, the library still requests the API for `UTC`, and then converts the ``luxon.DateTime`` objects to the desired timezone.
+
+Because most of the parameters are optional and Javascript does not support named function parameters, an object is passed as a single parameter.
+
+```javascript
+let forecast = await m.getPointForecast({
+    lat: 37.7775,  // Latitude of the point
+    lon: -122.416389,  // Longitude of the point
+    placeId: null,  // You can specify place_id instead of lat+lon
+    sections: ["current", "hourly"],  // is converted to "current,hourly"
+    tz: 'US/Pacific',
+    lang: 'en',
+    units: 'us',  // Defaults to 'auto'
+})
+
+// NOTE: The command above works in an async/wait context only
+// (async function, Node.JS REPL)
+// elsewhere you run:
+m.getPointForecast({...}).then(forecast => console.log(forecast))
+```
+
+### Historical weather
+Users with paid subscription to Meteosource can retrieve historical weather from the `time_machine` endpoint, using the `getTimeMachine()` method:
+
+```javascript
+let timeMachine = await m.getTimeMachine({
+    date: '2019-12-25',  // either a string, or a luxon.DateTime object or an array of these
+    dateFrom: null,  // You can specify the range for dates you need, instead of list
+    dateTo: null,  // You can specify the range for dates you need, instead of list
+    placeId: 'london', // ID of the place you want the historical weather for
+    lat: null,  // You can specify lat instead of placeId
+    lon: null,  // You can specify lon instead of placeId
+    tz: 'UTC',  // Defaults to 'UTC', regardless of the point location
+    units: 'us',  // Defaults to 'auto'
+})
+```
+Note that the historical weather data are always retrieved for full UTC days. If you specify a different timezone, the datetimes get converted, but they will cover the full UTC, not the local day.
+
+If you pass an array of dates to the `date` parameter, the days will be inserted into the inner structures in the order they are being iterated over. This affects time indexing by integer (see below). An API request is made for each day, even when you specify a date range.
+
+## Working with the weather data
+All of the data objects have overloaded `toString()` methods, so you can use them get useful information about the objects:
+
+```javascript
+console.log(forecast.toString())  // <Forecast for lat: 37.7775N, lon: 122.416389W>
+console.log(timeMachine.toString())  // <TimeMachine for lat: 51.50853N, lon: 0.12574W>
+```
+
+### Attribute access
+
+The methods ``getPointForecast`` and ``getTimeMachine`` return an object structure which is equivalent to the JSON response of the API (see the docs for the appropriate tier).
+
+You can access the attributes using the dot operator (`.`), or the index operator (`[]`):
+
+```javascript
+// You can access all of the attributes with dot operator:
+forecast.lat  // "37.7775N"
+
+// ... or with index operator:
+forecast['lon']  // "122.416389W"
+
+// There is also information about the elevation of the point and the timezone
+timeMachine.elevation  // 82
+timeMachine.timezone  // 'utc'
+```
+
+### Weather data sections
+
+There are 5 weather forecast sections (`current`, `minutely`, `hourly`, `daily` and `alerts`) as attributes in the root object from ``getPointForecast``.
+
+The `current` data contains data for many variables for a single point in time:
+
+```javascript
+console.log(forecast.current)
+```
+
+The `minutely`, `hourly` and `daily` sections contain forecasts for more points in time. The sections that were not requested are empty (set to ``null``):
+
+```javascript
+console.log(forecast.minutely)  // null
+console.log(forecast.daily)  // null
+```
+
+The sections that were requested can also be printed, to view number of available timesteps and their range (inclusive):
+
+```javascript
+console.log(forecast.hourly.toString())  // <Hourly data with 162 timesteps from 2022-05-20T11:00:00 to 2022-05-27T04:00:00
+```
+
+The `alerts` section contain meteorological alerts and warnings, if there are any issued for the location. You can print the object or iterate over it:
+```javascript
+console.log(forecast.alerts.toString())  // <Alerts (2 alerts available>
+for(let alert of forecast.alerts.data)
+    console.log(alert)
+```
+
+There is a single section `data` for historical weather in the root object returned by ``getTimeMachine``.
+
+```javascript
+console.log(timeMachine.data.toString())  // <TimeMachine data with 24 steps from 2019-12-25T00:00:00 to 2019-12-25T23:00:00
+```
+
+### Time indexing
+
+As mentioned above, the ``data`` objects contain data for more timesteps. To get the data for a single time, you have two options.
+
+  **1. Indexing with integer**
+
+You can simply index the ``data`` objects with `int`, as the offset from the current time:
+
+```javascript
+forecast.hourly.data[0]
+timeMachine.data[0]
+```
+
+  **2. Indexing with `datetime` or a string**
+
+You can also use `luxon.DateTime` as a parameter to the function ``getData``, the time zone of the object does not matter. A string datetime in the ISO format can be also used as the parameter, in such a case it is converted to ``luxon.DateTime``.
+
+The ``Datetime`` object does not have to be padded to the exact start of an hour (or a minute or a day).
+
+```javascript
+// get data for the current hour
+forecast.hourly.getData(luxon.DateTime.now())
+
+// for another hour -- beware, without an explicit timezone specification the local
+// timezone is used
+forecast.hourly.getData(luxon.DateTime.fromISO("2022-03-03T01:00:00")) // local TZ
+forecast.hourly.getData(luxon.DateTime.fromISO("2022-03-03T01:00:00Z")) // UTC
+forecast.hourly.getData(luxon.DateTime.fromISO("2022-03-03T01:00:00", {zone: "Europe/Prague"})) // a specific time zone
+
+// is equivalent to the previous UTC case
+forecast.hourly.getData("2022-03-03T01:00:00Z")
+
+// get historical weather
+timeMachine.getData(luxon.DateTime.fromISO("2019-12-25T01:00:00"))
+```
+
+
+### Variable access
+
+To access the variable, you can use the dot operator (`.`), or the index operator (`[]`):
+
+```javascript
+forecast.current.temperature
+forecast.hourly.data[0]['temperature']
+timeMachine.data[0]['weather']
+```
+
+Some variables are grouped into logical groups, just like in the API response. You can access the actual data with chained dot or index operators:
+
+```javascript
+Object.keys(forecast.current.wind)  // ['angle', 'dir', 'gusts', 'speed']
+forecast.current.wind.speed
+timeMachine.data[0]['wind'].dir  // WNW
+```
+
+
+### Contact us
+
+You can contact us [here](https://www.meteosource.com/contact).
---
+++ meteosource.js
@@ -0,0 +1,361 @@
+(function (root, factory) {
+
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['meteosource'], function() { return factory({}) });
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        // CommonJS
+        factory(exports);
+    } else {
+        // Browser globals
+        factory((root.meteosource = {}));
+    }
+}(typeof self !== 'undefined' ? self : this, function (exports) {
+
+    class MeteosourceError {
+        constructor(msg) {
+            if(typeof msg === "string") {
+                this.code = -1
+                this.detail = msg
+            } else if(typeof msg == "object" && "detail" in msg) {
+                this.code = "code" in msg ? msg.code : -1
+                this.detail = msg.detail
+            } else {
+                this.code = -1
+                this.detail = msg.toString()
+            }
+        }
+    }
+
+    let tiersAvailable = ["free", "startup", "standard", "flexi", "premium"]
+    Object.freeze(tiersAvailable)
+
+    // determine function for an HTTP request
+    let httpFunc
+    if (typeof fetch === "function") {
+        httpFunc = async function (url) {
+            let res
+            try {
+                res = await fetch(url)
+            } catch (e) {
+                throw {code: -1, detail: e.toString()}
+            }
+            let statusCode = res.status
+            let jsonRoot
+            try {
+                jsonRoot = await res.json()
+            } catch (e) {
+                try {
+                    jsonRoot = {detail: await res.text()}
+                } catch (e) {
+                    jsonRoot = {detail: "Unknown error"}
+                }
+            }
+            if (statusCode >= 200 && statusCode <= 299) {
+                return jsonRoot
+            } else {
+                jsonRoot.code = statusCode
+                throw new MeteosourceError(jsonRoot)
+            }
+        }
+    } else if(typeof require === "function") {
+        let https = require("https")
+        httpFunc = async function (url) {
+            let data = ""
+            let statusCode = -1
+            try {
+                data = await new Promise((resolve, reject) => {
+                    https.get(url, res => {
+                        statusCode = res.statusCode
+                        res.on("data", d => data = data + d)
+                        res.on("end", () => resolve(data))
+                    }).on("error", e => reject(e.message))
+                })
+            } catch(e) {
+                throw {code: statusCode, detail: e.toString()}
+            }
+
+            let jsonRoot
+            try {
+                jsonRoot = JSON.parse(data)
+            } catch(e) {
+                jsonRoot = {detail: data.toString()}
+            }
+
+            if (statusCode >= 200 && statusCode <= 299) {
+                return jsonRoot
+            } else {
+                jsonRoot.code = statusCode
+                throw jsonRoot
+            }
+        }
+    } else {
+        throw new MeteosourceError("cannot use HTTP request methods; supporting fetch() or require('https')")
+    }
+
+    class Meteosource {
+        #initialized = false
+        #apiKey = null
+        #tier = null
+        #luxon = null
+        #baseUrl = null
+
+        constructor(apiKey, tier, baseUrl = "https://www.meteosource.com/api/v1/") {
+            if(typeof apiKey !== "string")
+                throw new MeteosourceError("wrong type of the apiKey parameter; is " + typeof apiKey)
+            if(typeof tier !== "string")
+                throw new MeteosourceError("wrong type of the tier parameter; is " + typeof tier)
+            if(typeof baseUrl !== "string")
+                throw new MeteosourceError("wrong type of the baseUrl parameter; is " + typeof tier)
+            if(apiKey === "")
+                throw new MeteosourceError("the apiKey parameter is empty")
+            if(!tiersAvailable.includes(tier))
+                throw new MeteosourceError("tier " + tier + " does not exist or is not supported")
+
+            // load luxon library
+            if(typeof window === "object" && typeof window.luxon === "object")
+                this.#luxon = window.luxon
+            else if(typeof require === "function")
+                this.#luxon = require("luxon")
+            else
+                throw new MeteosourceError("luxon library is required")
+
+            this.#apiKey = apiKey
+            this.#tier = tier
+            this.#baseUrl = baseUrl
+            this.#initialized = true
+        }
+
+        // TODO docs
+        async getPointForecast(options) {
+            this.#checkInitialized()
+            this.#checkLimitedOptions(options, ["lat", "lon", "placeId", "sections", "tz", "lang", "units"])
+            let url = this.#baseUrl + this.#tier + "/point"
+            let sectionsArg
+            if(Array.isArray(options.sections))
+                sectionsArg = options.sections.join(",")
+            else
+                sectionsArg = options.sections
+            let args = {lat: options.lat, lon: options.lon, place_id: options.placeId, sections: sectionsArg,
+                        timezone: "utc", language: options.lang, units: options.units}
+
+            let res = await this.#httpComposed(url, args)
+
+            let usedTimezone = options.tz ? options.tz : "UTC"
+            res.toString = function() {
+                return "<Forecast for lat: " + res.lat + ", lon: " + res.lon + ">"
+            }
+            if(res.hourly !== null) {
+                res.hourly.toString = function() {
+                    return "<Hourly data with " + res.hourly.data.length + " timesteps from " +
+                        res.hourly.data[0].date.toISO().substr(0, 19) + " to " + res.hourly.data[res.hourly.data.length-1].date.toISO().substr(0, 19)
+                }
+                let hourStr2hourData = {}
+                let luxon = this.#luxon
+                res.hourly.data.forEach(hourData => {
+                    hourStr2hourData[hourData.date] = hourData
+                    hourData.date = this.#convertCheckDateTime(hourData.date, true).setZone(usedTimezone)
+                })
+                res.hourly.getData = hourDate => {
+                    hourDate = this.#convertCheckDateTime(hourDate, false)
+                    return hourStr2hourData[hourDate.setZone("UTC").startOf("hour").toISO().substr(0, 19)]
+                }
+            }
+            if(res.daily !== null) {
+                res.daily.toString = function() {
+                    return "<Daily data with " + res.daily.data.length + " steps from " +
+                        res.daily.data[0].day.toISO().substr(0, 10) +
+                        " to " + res.daily.data[res.daily.data.length-1].day.toISO().substr(0, 10)
+                }
+                let dayStr2dayData = {}
+                let luxon = this.#luxon
+                res.daily.data.forEach(dayData => {
+                    dayStr2dayData[dayData.day] = dayData
+                    dayData.day = luxon.DateTime.fromISO(dayData.day, {zone: usedTimezone})
+                })
+                res.daily.getData = day => {
+                    day = this.#convertCheckDateTime(day, false)
+                    return dayStr2dayData[day.toISO().substr(0, 10)]
+                }
+            }
+            if(res.minutely !== null) {
+                let luxon = this.#luxon
+                let str2minuteData = {}
+                res.minutely.data.forEach(minuteData => {
+                    str2minuteData[minuteData.date] = minuteData
+                    minuteData.date = luxon.DateTime.fromISO(minuteData.date + "Z", {zone: usedTimezone})
+                })
+                res.minutely.toString = function() {
+                    return "<Minutely data with " + res.minutely.data.length + " timesteps from " +
+                        res.minutely.data[0].date.toISO().substr(0, 19) + " to " +
+                        res.minutely.data[res.minutely.data.length-1].date.toISO().substr(0, 19)
+                }
+                res.minutely.getData = date => {
+                    date = this.#convertCheckDateTime(date, false)
+                    return str2minuteData[date.setZone("UTC").startOf("minute").toISO().substr(0, 19)]
+                }
+            }
+            if(res.current !== null) {
+                res.current.toString = function() {
+                    return "<Current data>"
+                }
+            }
+            if(res.alerts !== null) {
+                res.alerts.data.forEach(alertData => {
+                    alertData.onset = this.#luxon.DateTime.fromISO(alertData.onset + "Z", {zone: usedTimezone})
+                    alertData.expires = this.#luxon.DateTime.fromISO(alertData.expires + "Z", {zone: usedTimezone})
+                })
+                res.alerts.toString = function() {
+                    return "<Alerts (" + res.alerts.data.length + " alerts available>"
+                }
+                res.alerts.getActiveAlerts = date => {
+                    if(date === null || date === undefined)
+                        date = this.#luxon.DateTime.now()
+                    date = this.#convertCheckDateTime(date, false)
+                    return res.alerts.data.filter(alert => alert.onset <= date && date <= alert.expires)
+                }
+            }
+            return res
+        }
+
+        #convertCheckDateTime(date, forceUTCString) {
+            let utcPostfix = forceUTCString ? "Z" : ""
+            if(typeof date === "string")
+                date = this.#luxon.DateTime.fromISO(date + utcPostfix)
+
+            if(!this.#luxon.DateTime.isDateTime(date))
+                throw new MeteosourceError("either a string (DateTime ISO format like YYYY-MM-DDTHH:mm:ss) or a luxon.DateTime required")
+            else if(!date.isValid)
+                throw new MeteosourceError("passed DateTime (or string) is not valid")
+            else
+                return date
+        }
+
+        async getTimeMachine(options) {
+            this.#checkInitialized()
+            this.#checkLimitedOptions(options, [
+                "date", "dateFrom", "dateTo", "lat", "lon", "placeId", "tz", "units", "progressFunc", "strictMode"
+            ])
+            let url = this.#baseUrl + this.#tier + "/time_machine"
+
+            let date, dateFrom, dateTo
+            if(Array.isArray(options.date))
+                date = options.date.map(d => this.#convertCheckDateTime(d, false).toISO().substr(0, 10))
+            else if(options.date !== null && options.date !== undefined)
+                date = this.#convertCheckDateTime(options.date, false).toISO().substr(0, 10)
+            else
+                date = options.date
+            if(options.dateFrom !== null && options.dateFrom !== undefined)
+                dateFrom = this.#convertCheckDateTime(options.dateFrom, false).toISO().substr(0, 10)
+            else
+                dateFrom = options.dateFrom
+            if(options.dateTo !== null && options.dateTo !== undefined)
+                dateTo = this.#convertCheckDateTime(options.dateTo, false).toISO().substr(0, 10)
+            else
+                dateTo = options.dateTo
+
+            let datesToLoad = []
+            if(dateFrom && dateTo && !date) {
+                let dateFromTime = new Date(dateFrom).getTime()
+                let dateToTime = new Date(dateTo).getTime()
+                if(dateFromTime > dateToTime)
+                    throw new MeteosourceError("dateFrom must be lower or equal to dateTo")
+                for(let d = dateFromTime; d <= dateToTime; d += 24 * 3600 * 1000)
+                    datesToLoad.push(new Date(d).toISOString().split("T")[0])
+            }
+            else if(!dateFrom && !dateTo && date) {
+                if(Array.isArray(date))
+                    datesToLoad = date
+                else
+                    datesToLoad.push(date)
+            } else {
+                throw new MeteosourceError("either date, or dateFrom+dateTo parameters must be specified")
+            }
+
+            if(datesToLoad.length === 0)
+                throw new MeteosourceError("no dates range to load")
+
+            let completeRes = null
+            let dateIdx = 0
+            let failedDates = []
+            let lastError = null
+            for(const date of datesToLoad) {
+                let args = {date: date, lat: options.lat, lon: options.lon, place_id: options.placeId,
+                    timezone: "utc", units: options.units}
+                if(options.progressFunc)
+                    options.progressFunc(Math.round((dateIdx++/datesToLoad.length) * 100))
+                let res
+                try {
+                    res = await this.#httpComposed(url, args)
+                } catch(e) {
+                    if(!("strictMode" in options) || options.strictMode)
+                        throw e
+                    else {
+                        failedDates.push(date)
+                        lastError = e
+                        continue
+                    }
+                }
+                if(completeRes === null) {
+                    completeRes = res
+                } else {
+                    for (const dateItem of res.data)
+                        completeRes.data.push(dateItem)
+                }
+            }
+            if(options.progressFunc)
+                options.progressFunc(100)
+            if(completeRes === null)
+                throw lastError
+            completeRes.failedDates = failedDates
+
+            let hourStr2hourData = {}
+            let usedTimezone = options.tz ? options.tz : "UTC"
+            for(const dateItem of completeRes.data) {
+                hourStr2hourData[dateItem.date] = dateItem
+                dateItem.date = this.#convertCheckDateTime(dateItem.date, true).setZone(usedTimezone)
+            }
+            completeRes.getData = hourDate => {
+                hourDate = this.#convertCheckDateTime(hourDate, false)
+                return hourStr2hourData[hourDate.setZone("UTC").startOf("hour").toISO().substr(0, 19)]
+            }
+            completeRes.toString = function() {
+                return "<TimeMachine for lat: " + completeRes.lat + ", lon: " + completeRes.lon + ">"
+            }
+            completeRes.data.toString = function() {
+                return "<TimeMachine data with " + completeRes.data.length + " steps from " + completeRes.data[0].date.toISO().substr(0, 19) +
+                    " to " + completeRes.data[completeRes.data.length-1].date.toISO().substr(0, 19)
+            }
+            return completeRes
+        }
+        #httpComposed(url, args) {
+            let argsCleaned = "?key=" + this.#apiKey
+            Object.keys(args).forEach(key => {
+                if(args[key] !== null && args[key] !== undefined) {
+                    argsCleaned += "&" + key + "=" + args[key].toString()
+                }
+            })
+            return httpFunc(url + argsCleaned)
+        }
+
+        #checkLimitedOptions(options, optionsAllowed) {
+            let optionsAllowedHash = {}
+            optionsAllowed.forEach(opt => optionsAllowedHash[opt] = 1)
+            Object.keys(options).forEach(opt => {
+                if(!optionsAllowedHash[opt])
+                    throw new MeteosourceError("cannot use option '" + opt + "'")
+            })
+        }
+
+        #checkInitialized() {
+            if(!this.#initialized)
+                throw new MeteosourceError("cannot use, there was an error in the constructor")
+        }
+    }
+
+    exports.Meteosource = Meteosource
+    exports.version = "0.1"
+    exports.tiersAvailable = tiersAvailable
+
+    return exports
+}));
